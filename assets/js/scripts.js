6a5619006fbef41e82836a59b31bc1c84a9fac2b
AdamBoley <adamboley92@gmail.com>
2022-03-23 11:41:34 +0000

    add remaining logic for determinability, limits and output - logic related to drop-down menu still not working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -115,7 +115,7 @@ function calculateFinalSuspended(kv1, kv2) {
 
     document.getElementById('final-calculated-viscosity').innerText = finalViscosity;
 
-    determinabilityFactor()
+    determinabilityFactor(finalViscosity)
 }
 
 //Cross Arm viscomter / Zeitfuchs viscometer logic
@@ -195,58 +195,206 @@ function calculateFinalCrossArm(kv1, kv2) {
 
     document.getElementById('final-calculated-viscosity').innerText = finalViscosity
 
-    determinabilityFactor()
+    determinabilityFactor(finalViscosity)
 }
 
 //Problematic - only triggers the first option, no matter the option selected
-//perhaps change data-determinability-factor attribute values
+//with an else statement, defaults to the else command
+//perhaps change data-determinability-factor attribute values - no
 function displayDeterminabilityFactor() {
     
     let options = document.getElementsByTagName('option')
 
     for(let option of options) {
 
-        if(option.getAttribute('data-determinability-factor') === '0.0037') {
-        document.getElementById('determinability-factor').textContent = '0.0037 x Final calculated viscosity, or 0.37%'
+        if(option.getAttribute('value') === '0.0037') {
+
+            let message = '0.0037 x Final calculated viscosity, or 0.37%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.0036') {
-            document.getElementById('determinability-factor').textContent = '0.0036 x final calculated visocosity, or 0.36%'
+        else if(option.getAttribute('value') === '0.0036') {
+
+            let message = '0.0036 x final calculated visocosity, or 0.36%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.0150') {
-            document.getElementById('determinability-factor').textContent = '0.015 x final calculated visocosity or 1.5%'
+        else if(option.getAttribute('value') === '0.015') {
+
+            let message = '0.015 x final calculated visocosity or 1.5%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.0080') {
-            document.getElementById('determinability-factor').textContent = '0.0080 x final calculated visocosity, or 0.80%'
+        else if(option.getAttribute('value') === '0.0080') {
+
+            let message = '0.0080 x final calculated visocosity, or 0.80%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.0244') {
-            document.getElementById('determinability-factor').textContent = '0.0244 x final calculated visocosity, or 2.44%'
+        else if(option.getAttribute('value') === '0.0244') {
+
+            let message = '0.0244 x final calculated visocosity, or 2.44%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.0300') {
-            document.getElementById('determinability-factor').textContent = '0.03 x final calculated visocosity, or 3%'
+        else if(option.getAttribute('value') === '0.03') {
+
+            let message = '0.03 x final calculated visocosity, or 3%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.00106^1.1') {
-            document.getElementById('determinability-factor').textContent = '0.00106^1.1 x final calculated visocosity'
+        else if(option.getAttribute('value') === '0.00106^1.1') {
+
+            let message = '0.00106^1.1 x final calculated viscosity'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.0013(y+1)') {
-            document.getElementById('determinability-factor').textContent = '0.0013 x (final calculated viscosity +1)'
+        else if(option.getAttribute('value') === '0.0013(y+1)') {
+
+            let message = '0.0013 x (final calculated viscosity +1)'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
-        else if(option.getAttribute('data-determinability-factor') === '0.007608') {
-            document.getElementById('determinability-factor').textContent = '0.007608 x final calculated visocosity, or 0.7608%'
+        else if(option.getAttribute('value') === '0.007608') {
+
+            let message = '0.007608 x final calculated visocosity, or 0.7608%'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
         }
 
+        else {
+
+            let message = 'invalid determinability equation'
+            document.getElementById('determinability-equation').innerText = message
+            console.log(message)
+        }
         //possibly use template literals here to call the data-determinability-factor attribute value and final calculated viscosity
-        
-
     }
 }
+
+//These are sort of working, except that the functions just call all of the options, and are not able to discriminate between the if statements
+function determinabilityFactor(finalViscosity) {
+
+    let options = document.getElementsByTagName('option')
+
+    for(let option of options) {
+
+        if(option.getAttribute('value') === '0.0037') {
+            
+            let determinability = finalViscosity * 0.0037
+            console.log(`${finalViscosity} x 0.0037 = `, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.0036') {
+            let determinability = finalViscosity * 0.0036
+            console.log(`${finalViscosity} x 0.0036 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.015') {
+            let determinability = finalViscosity * 0.015
+            console.log(`${finalViscosity} x 0.015 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.0080') {
+            let determinability = finalViscosity * 0.0080
+            console.log(`${finalViscosity} x 0.0080 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.0244') {
+            let determinability = finalViscosity * 0.0244
+            console.log(`${finalViscosity} x 0.0244 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.0300') {
+            let determinability = finalViscosity * 0.03
+            console.log(`${finalViscosity} x 0.03 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.00106^1.1') {
+            let determinability = (finalViscosity ** 1.1) * 0.00106
+            console.log(`(${finalViscosity} ^1.1) x 0.00106 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.0013(y+1)') {
+            let determinability = (finalViscosity + 1) * 0.0013
+            console.log(`(${finalViscosity} + 1) x 0.013 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+
+        else if(option.getAttribute('value') === '0.007608') {
+            let determinability = finalViscosity * 0.007608
+            console.log(`${finalViscosity} x 0.007608 =`, determinability)
+            document.getElementById('determinability-factor').innerText = determinability
+        }
+    }
+
+    //upperLimit(finalViscosity, determinability)
+    //lowerLimit(finalViscosity, determinability)
+    upperLimit()
+    lowerLimit()
+}
+
+function upperLimit() {
+
+    let finalViscosity = parseFloat(document.getElementById('final-calculated-viscosity').innerText)
+    let determinabilityFactor = parseFloat(document.getElementById('determinability-factor').innerText)
+
+    let upperAllowedViscosity = finalViscosity + determinabilityFactor
+
+    document.getElementById('upper-limit').innerText = upperAllowedViscosity
+
+    console.log(`The upper allowed viscosity is ${upperAllowedViscosity}`)
+
+}
+
+function lowerLimit() {
+
+    let finalViscosity = parseFloat(document.getElementById('final-calculated-viscosity').innerText)
+    let determinabilityFactor = parseFloat(document.getElementById('determinability-factor').innerText)
+
+    let lowerAllowedViscosity = finalViscosity - determinabilityFactor
+
+    document.getElementById('lower-limit').innerText = lowerAllowedViscosity
+
+    console.log(`The lower allowed viscosity is ${lowerAllowedViscosity}`)
+
+    checker()
+}
+
+function checker() {
+
+    let viscosity1 = parseFloat(document.getElementById('kinematic-viscosity-1').innerText)
+    let viscosity2 = parseFloat(document.getElementById('kinematic-viscosity-2').innerText)
+    let upperAllowedViscosity = parseFloat(document.getElementById('upper-limit').innerText)
+    let lowerAllowedViscosity = parseFloat(document.getElementById('lower-limit').innerText)
+
+    if(viscosity1 > lowerAllowedViscosity && viscosity1 < upperAllowedViscosity && viscosity2 > lowerAllowedViscosity && viscosity2 < upperAllowedViscosity) {
+
+        document.getElementById('output').innerText = "Your viscosities fall within the defined limits"
+    }
+
+    else {
+        document.getElementById('output').innerText = "Your viscosities do not fall within the defined limits"
+    }
+
+}
+
+
 
 
 
