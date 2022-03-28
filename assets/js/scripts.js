4d0bb1aeaec200f9315714d52010634f5f0d7b78
AdamBoley <adamboley92@gmail.com>
2022-03-28 15:53:44 +0000

    add html and JS functions for viscometer calibration calculations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1143,6 +1143,132 @@ function reproducibilityReset() {
     document.getElementById('viscosity-reproducibility-1').focus()
 }
 
+//Calibration calculations
+
+let calibrationCalculateButton = document.getElementById('submit-calibration')
+
+calibrationCalculateButton.addEventListener('click', getValuesCalibration)
+
+function getValuesCalibration() {
+
+    let calibrationRunTime1 = parseFloat(document.getElementById('calibration-run-time-1').value)
+    let calibrationRunTime2 = parseFloat(document.getElementById('calibration-run-time-2').value)
+    let calibrationConstant = parseFloat(document.getElementById('calibration-constant').value)
+
+    console.log(`run time 1 is ${calibrationRunTime1}`)
+    console.log(`run time 2 is ${calibrationRunTime2}`)
+    console.log(`the viscometer constant is ${calibrationConstant}`)
+
+    calculateCalibration(calibrationRunTime1, calibrationRunTime2, calibrationConstant)
+
+}
+
+function calculateCalibration(calibrationRunTime1, calibrationRunTime2, calibrationConstant) {
+
+    let averageRunTime = ((calibrationRunTime1 + calibrationRunTime2) / 2)
+    let viscosity1 = calibrationRunTime1 * calibrationConstant
+    let viscosity2 = calibrationRunTime2 * calibrationConstant
+    let averageViscosity = ((viscosity1 + viscosity2) / 2)
+
+    console.log(`the average run time is ${averageRunTime}`)
+    console.log(`viscosity 1 is ${viscosity1}`)
+    console.log(`viscosity 2 is ${viscosity2}`)
+    console.log(`the average viscosity is ${averageViscosity}`)
+
+    document.getElementById('calibration-average-run-time').textContent = averageRunTime
+    document.getElementById('calibration-average-viscosity').textContent = averageViscosity
+
+    tolerance(averageViscosity)
+
+}
+
+function tolerance(averageViscosity) {
+
+    let calibrationFluidViscosity = document.getElementById('calibration-fluid-viscosity').value
+    
+    switch (true) {
+        case calibrationFluidViscosity < 10 :
+            document.getElementById('calibration-viscosity-range').textContent = '<10'
+            document.getElementById('calibration-tolerance-band').textContent = '+/- 0.30%'
+            toleranceBand = 0.30
+            break
+        
+        case calibrationFluidViscosity >= 10 && calibrationFluidViscosity < 100:
+            document.getElementById('calibration-viscosity-range').textContent = '10 to 100'
+            document.getElementById('calibration-tolerance-band').textContent = '+/- 0.32%'
+            toleranceBand = 0.32
+            break
+        
+        case calibrationFluidViscosity >= 100 && calibrationFluidViscosity < 1000 :
+            document.getElementById('calibration-viscosity-range').textContent = '100 to 1000'
+            document.getElementById('calibration-tolerance-band').textContent = '+/- 0.36%'
+            toleranceBand = 0.36
+            break
+
+        case calibrationFluidViscosity >= 1000 && calibrationFluidViscosity < 10000 :
+            document.getElementById('calibration-viscosity-range').textContent = '1000 to 10000'
+            document.getElementById('calibration-tolerance-band').textContent = '+/- 0.42%'
+            toleranceBand = 0.42
+            break
+
+        case calibrationFluidViscosity >= 10000 && calibrationFluidViscosity < 100000 :
+            document.getElementById('calibration-viscosity-range').textContent = '10000 to 100000'
+            document.getElementById('calibration-tolerance-band').textContent = '+/- 0.54%'
+            toleranceBand = 0.54
+            break
+
+        case calibrationFluidViscosity >= 100000 :
+            document.getElementById('calibration-viscosity-range').textContent = '>100000'
+            document.getElementById('calibration-tolerance-band').textContent = '+/- 0.73%'
+            toleranceBand = 0.73
+            break
+    }
+    
+    percentageDifference(calibrationFluidViscosity, averageViscosity, toleranceBand)
+}
+
+function percentageDifference(calibrationFluidViscosity, averageViscosity, toleranceBand) {
+
+    let numerator = calibrationFluidViscosity - averageViscosity
+    numerator = Math.abs(numerator)
+
+    let denominator = ((calibrationFluidViscosity + averageViscosity) / 2)
+
+    let percentageDifference = (numerator / denominator) * 100
+
+    document.getElementById('calibration-percentage-difference').textContent = percentageDifference
+
+    percentageDifferenceChecker(percentageDifference, toleranceBand)
+}
+
+function percentageDifferenceChecker(percentageDifference, toleranceBand) {
+
+    if(percentageDifference <= toleranceBand) {
+        document.getElementById('calibration-output').textContent = 'percentage difference is less than or equal to tolerance band. Viscometer passes calibration check'
+    }
+    else if(percentageDifference > toleranceBand) {
+        document.getElementById('calibration-output').textContent = 'percentage difference is greater than tolerance band. Viscometer fails calibration check'
+    }
+}
+
+let calibrationResetButton = document.getElementById('calibration-reset')
+
+calibrationResetButton.addEventListener('click', calibrationReset)
+
+function calibrationReset() {
+
+    document.getElementById('calibration-run-time-1').value = ''
+    document.getElementById('calibration-run-time-2').value = ''
+    document.getElementById('calibration-constant').value = ''
+    document.getElementById('calibration-fluid-viscosity').value = ''
+    document.getElementById('calibration-average-run-time').textContent = ''
+    document.getElementById('calibration-average-viscosity').textContent = ''
+    document.getElementById('calibration-viscosity-range').textContent = ''
+    document.getElementById('calibration-tolerance-band').textContent = ''
+    document.getElementById('calibration-percentage-difference').textContent = ''
+    document.getElementById('calibration-output').textContent = ''
+    document.getElementById('calibration-run-time-1').focus()
+}
 
 
 
