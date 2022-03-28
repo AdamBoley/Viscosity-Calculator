fb398d9822742a2b3db59a5f54144c79d7b29dd8
AdamBoley <adamboley92@gmail.com>
2022-03-28 16:57:43 +0000

    add html and JS functions for the calculations in the recalibration of viscometer constants

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1270,7 +1270,63 @@ function calibrationReset() {
     document.getElementById('calibration-run-time-1').focus()
 }
 
+//Viscometer Recalibration calculations
 
+let recalibrationButton = document.getElementById('submit-recalibration')
+
+recalibrationButton.addEventListener('click', recalibrationPercentageDifference)
+
+function recalibrationPercentageDifference() {
+
+    let testingGravity = parseFloat(document.getElementById('testing-lab-gravity').value)
+    let standardisationGravity = parseFloat(document.getElementById('standardisation-lab-gravity').value)
+    let constant = parseFloat(document.getElementById('recalibration-constant').value)
+
+    console.log(`the gravity at the testing lab is ${testingGravity}`)
+    console.log(`the gravity at the standardisation lab is ${standardisationGravity}`)
+    console.log(`the constant of the viscometer is ${constant}`)
+
+    let numerator = testingGravity - standardisationGravity
+    numerator = Math.abs(numerator)
+
+    console.log(numerator)
+
+    let denominator = ((testingGravity + standardisationGravity) / 2)
+
+    console.log(denominator)
+
+    let percentageDifference = (numerator / denominator) * 100
+    let precisePercentageDifference = percentageDifference.toPrecision(4)
+
+    document.getElementById('recalibration-percentage-difference').textContent = precisePercentageDifference
+    console.log(`the percentage difference is ${precisePercentageDifference}`)
+
+    recalibrationFunction(percentageDifference, testingGravity, standardisationGravity, constant)
+}
+
+function recalibrationFunction(percentageDifference, testingGravity, standardisationGravity, constant) {
+
+    if(percentageDifference > 0.1) {
+        let newConstant = (standardisationGravity / testingGravity) * constant
+        let preciseNewConstant = newConstant.toPrecision(4)
+        console.log(`the new constant of the viscometer is ${preciseNewConstant}`)
+        document.getElementById('new-constant').textContent = preciseNewConstant
+    }
+    else {
+        document.getElementById('new-constant').textContent = 'the difference between the two gravities does not warrant recalibration of the viscometer'
+    }
+}
+
+let recalibrationResetButton = document.getElementById('recalibration-reset')
+recalibrationResetButton.addEventListener('click', recalibrationReset)
+
+function recalibrationReset() {
+    document.getElementById('recalibration-constant').value = ''
+    document.getElementById('testing-lab-gravity').value = ''
+    document.getElementById('standardisation-lab-gravity').value = ''
+    document.getElementById('recalibration-percentage-difference').textContent = ''
+    document.getElementById('new-constant').textContent = ''
+}
 
 //Do I want to use the outputs of the toPrecision method in the final calculations? There are edge cases where I round my number throughout the process and come out with a 
 //slightly different value to what GLIMS calculates, because GLIMS presumably doesn't round. 
